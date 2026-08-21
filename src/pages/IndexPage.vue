754fd0c98e895e3166cd4118edb7b26a37993c94
นายเอกราช แซ่ว่าง <dgzoqr@gmail.com>
2026-08-21 14:32:40 +0700

    fix-duplicate-form-fields-400

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,14 @@
 <template>
   <q-page class="q-pa-md">
-    <q-form class="q-gutter-md" style="max-width: 600px">
-
-      <q-input filled label="Your name (ชื่อ-นามสกุล)*" hint="Name and surname" />
-      <q-input filled type="number" label="Your age (อายุ)*" />
-      <q-toggle label="I accept the license and terms" />
-
-      <q-input filled label="Your name *" hint="Name and surname" />
-      <q-input filled type="number" label="Your age *" />
-      <q-toggle v-model="accept" label="I accept the license and terms (我接受许可和条款语言)" />
-
-      <div>
-        <q-btn label="SUBMIT(提出する)" type="submit" color="primary" />
-        <q-btn label="RESET(リセット)" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
+<q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md" style="max-width: 600px">
+  <q-input filled v-model="name" label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
+  <q-input filled type="number" v-model="age" label="Your age (อายุ) *" />
+  <q-toggle v-model="accept" label="I accept the license and terms (我接受许可和条款语言)" />
+  <div>
+    <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
+    <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
+  </div>
+</q-form>
   </q-page>
 </template>
 <script setup>
